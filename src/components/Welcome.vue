484d675536a98e8378144f08d6e3049b1a356c6b
Williams Alvarez - ADMISION <williamswaz18@gmail.com>
2025-09-25 17:38:49 -0400

    Welcome Component created

---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<script>
+    export default{
+        name: 'welcomeApp',
+        description: "Main page for app",
+        props: {
+            message: {
+                type: String, 
+                default: "Bienvenido",
+            },
+        }
+    }
+</script>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <Welcome message="Bienvenida, Doctora"/>
+    <img alt="logo Dra. Cecilia Diaz" src="../assets/logoDra.png">
+</template>
+
+<script>
+    import Welcome from '@/components/Welcome.vue'
+    export default{
+        components: { Welcome },
+    }
+</script>
