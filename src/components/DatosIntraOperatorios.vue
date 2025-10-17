12019a4947f3c8792a8e490323b2ea875286255a
Williams Alvarez Zabala <williamswaz18@gmail.com>
2025-10-16 22:48:55 -0400

    datosIntraOperatorios is done

--- src/components/DatosIntraOperatorios.vue
+++ src/components/DatosIntraOperatorios.vue
@@ -73,6 +73,11 @@
         </div>
 
         <div class="row mt-3">
+            <div class=" text-primary">
+                <h4>Tiempo quirurgico</h4>
+            </div>
+        </div>
+        <div class="row">
             <div class="col lg-6">
                 <div class="form-floating">
                     <input type="number" name="despertar" class="form-control" v-model="data.despertar">
@@ -85,6 +90,75 @@
                     <label for="tiempoQx">Tiempo quirurgico (min)</label>
                 </div>
             </div>    
+        </div> 
+        
+        <div class="row mt-3">
+            <div class=" text-primary">
+                <h4>Eventos Intra Operatorios</h4>
+            </div>
+        </div>
+        <div class="col lg-12">
+            <div class="row">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="checkPresionArterial" v-model="data.presionArterial">
+                    <label class="form-check-label" for="checkPresionArterial">
+                        Presion arterial (PA)
+                    </label>
+                    <div v-if="this.data.presionArterial === true" class="form-floating mt-2">
+                        <input type="text" name="valorPresionArterial" class="form-control" v-model="data.valorPresionArterial" placeholder="Valor de PA">
+                        <label for="valorPresionArterial">Valor presion arterial</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="checkFrecuenciaCardiaca" v-model="data.frecuenciaCardiaca">
+                    <label class="form-check-label" for="checkFrecuenciaCardiaca">
+                        Frecuencia cardiaca (FC)
+                    </label>
+                    <div v-if="this.data.frecuenciaCardiaca === true" class="form-floating mt-2">
+                        <input type="text" name="valorFrecuenciaCardiaca" class="form-control" v-model="data.valorFrecuenciaCardiaca" placeholder="Valor de FC">
+                        <label for="valorFrecuenciaCardiaca">Valor frecuencia cardiaca</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                    <div class="form-check">
+                         <input class="form-check-input" type="checkbox" value="" id="checkCo2" v-model="data.co2">
+                        <label class="form-check-label" for="checkCo2">
+                            Co2
+                    </label>
+                    <div v-if="this.data.co2 === true" class="form-floating mt-2">
+                        <input type="text" name="valorCo2" class="form-control" v-model="data.valorCo2" placeholder="Valor de Co2">
+                        <label for="valorCo2">Valor Co2</label>
+                    </div>
+                </div>
+            </div>    
+            <div class="row">
+                <div class="form-check">
+                     <input class="form-check-input" type="checkbox" value="" id="checkSatO2" v-model="data.satO2">
+                    <label class="form-check-label" for="checkSatO2">
+                        Saturacion O2
+                    </label>
+                    <div v-if="this.data.satO2 === true" class="form-floating mt-2">
+                        <input type="text" name="valorSatO2" class="form-control" v-model="data.valorSatO2" placeholder="Valor de Saturacion O2">
+                        <label for="valorSatO2">Valor Saturacion O2</label>
+                    </div>
+                </div>
+            </div>    
+            <div class="row">
+                <div class="form-check">
+                     <input class="form-check-input" type="checkbox" value="" id="checkFR" v-model="data.frecuenciaRespiratoria">
+                    <label class="form-check-label" for="checkFR">
+                        Frecuencia respiratoria (FR)
+                    </label>
+                    <div v-if="this.data.frecuenciaRespiratorias === true" class="form-floating mt-2">
+                        <input type="text" name="valorFR" class="form-control" v-model="data.valorFR" placeholder="Valor de FR">
+                        <label for="valorFR">Valor frecuencia respiratoria</label>
+                    </div>
+                </div>
+            </div>        
         </div> 
         
 
